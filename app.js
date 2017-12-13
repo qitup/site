aff31611dfd051dfcb9f2d0fb3a9be50cb80b561
TrevorFoster <trevor.foster@hotmail.ca>
2017-12-12 20:45:07 -0500

    Removed node-sass

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
-const sass = require('node-sass-middleware');
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -39,17 +38,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(expressStatusMonitor());
 app.use(compression());
-app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
-// app.use(lusca.xframe('SAMEORIGIN'));
-// app.use(lusca.xssProtection(true));
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
